7b73d8e078a9c60dcf18b804d5244f819e247132
neomoment-github <neomoment.ai@gmail.com>
2022-11-22 00:59:50 +0530

    working....

--- actions/actions.py
+++ actions/actions.py
@@ -1,14 +1,4 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
 from typing import Any, Text, Dict, List
-
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
@@ -25,3 +15,4 @@ class ActionHelloWorld(Action):
         dispatcher.utter_message(text="Hello World!")
 
         return []
+
